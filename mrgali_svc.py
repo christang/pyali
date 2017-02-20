6434fd8153e56d70da73ae24644c5458850c2205
Chris Tang <chris.l.tang@gmail.com>
2017-02-20 18:23:10 -0500

    add endpoint for computing alignment merge

---
+++ mrgali_svc.py
@@ -0,0 +1,36 @@
+import falcon
+import simplejson
+from mrgali import Alignment
+
+
+class AlignmentService:
+    def on_post(self, req, resp):
+        body = req.stream.read()
+        if not body:
+            raise falcon.HTTPBadRequest('bad request', 'no sequences')
+
+        try:
+            doc = simplejson.loads(body)
+        except KeyError:
+            raise falcon.HTTPBadRequest('bad request', 'no sequences')
+        except simplejson.JSONDecodeError:
+            raise falcon.HTTPBadRequest('bad request', 'cannot parse')
+
+        try:
+            ref_ali = doc['ref']
+            a = Alignment.from_reference(ref_ali)
+        except KeyError:
+            raise falcon.HTTPBadRequest('bad request', 'missing ref')
+
+        try:
+            seq_alis = doc['seqs']
+            for i, seq_ali in seq_alis:
+                a.merge(i, seq_ali)
+        except KeyError:
+            raise falcon.HTTPBadRequest('bad request', 'missing seqs')
+
+        resp.body = str(a)
+
+
+api = falcon.API()
+api.add_route('/merge', AlignmentService())
