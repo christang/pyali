d428fdc402ea22461cca897f4616a9020ad53afe
Chris Tang <chris.l.tang@gmail.com>
2017-11-15 18:08:45 -0500

    prepare to publish to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyali',
+  packages = ['pyali'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A package for merging alignments',
+  author = 'Chris L Tang',
+  author_email = 'chris.l.tang@gmail.com',
+  url = 'https://github.com/christang/pyali', # use the URL to the github repo
+  download_url = 'https://github.com/christang/pyali/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['bioinformatics', 'sequence', 'alignment'], # arbitrary keywords
+  classifiers = [],
+)
